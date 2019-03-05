a86ce930240fb0b5611bac0b49cc77ca8b398874
fgomez95 <fgomez_sierra95@gmail.com>
2019-03-04 21:29:39 -0600

    initialized project

---
+++ main.js
@@ -0,0 +1,56 @@
+// TS cheatsheet
+//boolean
+var myFirstBoolean = false;
+var mySecondBoolean = true;
+console.log("type boolean: " + myFirstBoolean + ", " + mySecondBoolean);
+//number 
+var myInt = 10;
+var myDecimal = 10.00;
+console.log("type number: " + myInt + ", " + myDecimal);
+//string 
+var myString = 'hello';
+var myMessage = 'world';
+console.log("type string: " + myString + ", " + myMessage + "!");
+//null and undefined
+var myUndefined = undefined;
+var myNull = null;
+console.log("types undefined and null: " + myUndefined + ", " + myNull);
+// simple array
+var myFirstArr = ['hello', 'world'];
+var mySecondArr = ['foo', 'bar'];
+console.log("type array: " + myFirstArr[0] + " " + mySecondArr[1]);
+//simple object
+var myObject = { message: 'foo' };
+console.log("type object: " + myObject.message);
+//tuple 
+var myTuple = ['Foo', 10];
+console.log("tuple: " + myTuple);
+//enum 
+var Status;
+(function (Status) {
+    Status[Status["on"] = 1] = "on";
+    Status[Status["off"] = 0] = "off";
+    Status[Status["sleep"] = 3] = "sleep";
+})(Status || (Status = {}));
+console.log(Status.on);
+//functions with return 
+function sayHello() { return 'hello'; }
+function getAge() { return 10; }
+console.log(sayHello() + ", my age is " + getAge());
+//void functions 
+function bar() { console.log('Bar'); }
+function foo(message) { console.log(message); }
+foo('fizz');
+bar();
+//never 
+function error() {
+    throw Error('unauthorized');
+}
+var dog = {
+    name: 'fizz',
+    woof: function () { console.log('woof!'); }
+};
+function dogShower(dog) {
+    dog.woof();
+}
+dogShower(dog);
---
+++ main.ts
@@ -0,0 +1,79 @@
+// TS cheatsheet
+
+//boolean
+const myFirstBoolean: boolean = false;
+const mySecondBoolean: boolean = true;
+
+console.log(`type boolean: ${myFirstBoolean}, ${mySecondBoolean}`);
+
+//number 
+const myInt: number = 10;
+const myDecimal: number = 10.00;
+
+console.log(`type number: ${myInt}, ${myDecimal}`);
+
+//string 
+const myString: string = 'hello';
+const myMessage: string = 'world';
+
+console.log(`type string: ${myString}, ${myMessage}!`);
+
+//null and undefined
+const myUndefined: undefined = undefined;
+const myNull: null = null;
+
+console.log(`types undefined and null: ${myUndefined}, ${myNull}`);
+
+// simple array
+const myFirstArr: string[] = ['hello', 'world'];
+const mySecondArr: Array<string> = ['foo', 'bar'];
+
+console.log(`type array: ${myFirstArr[0]} ${mySecondArr[1]}`);
+
+//simple object
+const myObject: { message?: string } = { message: 'foo' };
+console.log(`type object: ${myObject.message}`);
+
+//tuple 
+const myTuple: [string, number] = ['Foo', 10];
+console.log(`tuple: ${myTuple}`);
+
+//enum 
+enum Status { on = 1, off = 0, sleep = 3 }
+console.log(Status.on);
+
+//functions with return 
+function sayHello(): string { return 'hello'; }
+function getAge(): number { return 10; }
+
+console.log(`${sayHello()}, my age is ${getAge()}`);
+
+//void functions 
+function bar(): void { console.log('Bar'); }
+function foo(message: string): void { console.log(message); }
+
+foo('fizz');
+bar();
+
+//never 
+
+function error(): never {
+    throw Error('unauthorized');
+}
+
+//interface
+
+interface Dog {
+    name: string,
+    woof(): void,
+}
+
+const dog: Dog = {
+    name: 'fizz',
+    woof: function(){ console.log('woof!'); }
+}
+
+function dogShower(dog: Dog): void{
+    dog.woof();
+}
+dogShower(dog);
